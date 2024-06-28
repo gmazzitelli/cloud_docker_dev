3550da13d80ab68472a5abd81acd1a054f1be304
root <root@vnode-0.localdomain>
2024-06-28 15:35:36 +0000

    v26 / rados gatway

--- builder.sh
+++ builder.sh
@@ -2,8 +2,8 @@
 # docker labs/wn builder
 #
 TAGIN='v1.0.21'
-TAGOUT='v1.0.23'
-docker build -t gmazzitelli/cygno-wn:${TAGOUT}-cygno --build-arg TAG=cygno-wn:${TAGIN}-cygno .
-docker build -t gmazzitelli/cygno-lab:${TAGOUT}-cygno --build-arg TAG=cygno-lab:${TAGIN}-cygno .
+TAGOUT='v1.0.26'
+docker build -t gmazzitelli/cygno-wn:${TAGOUT}-cygno --build-arg TAG=cygno-wn:${TAGIN}-cygno -f Dockerfile.wn .
+docker build -t gmazzitelli/cygno-lab:${TAGOUT}-cygno --build-arg TAG='notag' -f Dockerfile.lab .
 docker push gmazzitelli/cygno-wn:${TAGOUT}-cygno
 docker push gmazzitelli/cygno-lab:${TAGOUT}-cygno
