e8d50a07bf6b718783c3ce073237cea498380158
Giovanni Mazzitelli <giovanni.mazzitelli@lnf.infn.it>
2024-09-18 08:35:51 +0000

    builder with cygnolib versionning

--- builder.sh
+++ builder.sh
@@ -2,8 +2,9 @@
 # docker labs/wn builder
 #
 TAGIN='v1.0.21'
-TAGOUT='v1.0.26'
-docker build -t gmazzitelli/cygno-wn:${TAGOUT}-cygno --build-arg TAG=cygno-wn:${TAGIN}-cygno -f Dockerfile.wn .
-docker build -t gmazzitelli/cygno-lab:${TAGOUT}-cygno --build-arg TAG='notag' -f Dockerfile.lab .
+TAGOUT='v1.0.27'
+CLIB='v1.0.18'
+docker build -t gmazzitelli/cygno-wn:${TAGOUT}-cygno --build-arg TAG=cygno-wn:${TAGIN}-cygno --build-arg CLIB=${CLIB} --no-cache -f Dockerfile.wn .
+docker build -t gmazzitelli/cygno-lab:${TAGOUT}-cygno --build-arg TAG='notag' --build-arg CLIB=${CLIB} --no-cache -f Dockerfile.lab .
 docker push gmazzitelli/cygno-wn:${TAGOUT}-cygno
 docker push gmazzitelli/cygno-lab:${TAGOUT}-cygno
